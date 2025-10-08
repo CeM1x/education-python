ff4342870eaf6b9f4439a56c5a3c1b2b5138ab43
CeM1x <108667250+CeM1x@users.noreply.github.com>
2025-10-08 10:39:58 +0300

    Update main.py

--- word_generator/main.py
+++ word_generator/main.py
@@ -4,22 +4,22 @@ from typing import Generator
 def read_file_by_words(file_name: str) -> Generator[str | list[str], int | None, None]:
     with open(file_name, "r") as file:
         words = (word for line in file for word in line.split())
-        try:
-            while True:
+        while True:
+            try:
                 word = next(words)
-                n = yield word
-                if n is not None:
-                    lst = [next(words) for _ in range(n)]
-                    yield lst
-        except StopIteration:
-            return
-
+            except StopIteration:
+                return
+            n = yield word
+            if n is not None:
+                lst = []
+                for _ in range(n):
+                    try:
+                        lst.append(next(words))
+                    except StopIteration:
+                        break
+                yield lst
 
 rfbw = read_file_by_words("text")
 print(rfbw)
-
-
-print(next(rfbw)) # Вывод: one
-print(rfbw.send(3)) # Вывод: ['two', 'three', 'four']
-print(next(rfbw)) # Вывод: five
-print(list(rfbw)) #Вывод: ['six', 'seven']
+print(next(rfbw))
+print(rfbw.send(5))
